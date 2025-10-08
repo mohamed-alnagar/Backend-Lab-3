53edca35776a9cad64f13842bc5a09c7000efab9
Alnagar <mohamedalnagar20@gmail.com>
2025-10-08 08:06:40 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require('dotenv')
-const usersRouter = require("./routers/users");
-const postsRouter = require("./routers/posts");
+const usersRouter = require("./routers/usersRouter");
+const postsRouter = require("./routers/postsRouter");
 const app=express();
 
 app.use(express.json());
